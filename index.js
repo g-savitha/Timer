d1038a76f6e2e9c9829164f29a94a796e47cce57
Savitha Gollamudi <savithagollamudi@github.com>
2020-06-25 21:51:36 +0530

    Boiler plate of timer

---
+++ index.js
@@ -0,0 +1,18 @@
+class Timer {
+  constructor(durationInput, startBtn, pauseBtn) {
+    this.durationInput = durationInput;
+    this.startBtn = startBtn;
+    this.pauseBtn = pauseBtn;
+
+    this.startBtn.addEventListener("click", this.start);
+  }
+  start() {
+    console.log("Time to start timer");
+  }
+}
+
+const durationInput = document.querySelector("#duration");
+const startBtn = document.querySelector("#start");
+const pauseBtn = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
