404f3fe1c08c13d3ce3cce01508fcf7c4593506c
Savitha Gollamudi <savithagollamudi@github.com>
2020-06-26 18:37:01 +0530

    Added callbacks to notify for future events

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 class Timer {
-  constructor(durationInput, startBtn, pauseBtn) {
+  constructor(durationInput, startBtn, pauseBtn, callbacks) {
     this.durationInput = durationInput;
     this.startBtn = startBtn;
     this.pauseBtn = pauseBtn;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.startBtn.addEventListener("click", this.start);
     this.pauseBtn.addEventListener("click", this.pause);
   }
   start = () => {
-    this.tick; // run 1 tick to coverup 1 sec delay to start
-    //assign timer variable to `this` so we can access it across other methods
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick;
     this.intervalId = setInterval(this.tick, 1000);
   };
   tick = () => {
-    //automatically invokes the getter function even though we dont have paranthesis at end.
-    //treats it as instance variable, coz of 'get' keyword
-    // const timeRemaining = this.timeRemaining;
-    // this.timeRemaining = timeRemaining - 1;
-    //in short
-    this.timeRemaining = this.timeRemaining - 1; //left side one calls setter, right side one calls getter;
+    //stop the timer when it hits 0, else continue
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) this.onComplete();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
   };
   pause = () => {
     clearInterval(this.intervalId);
   };
-  //getter
   get timeRemaining() {
     return parseFloat(this.durationInput.value);
   }
-  //setter
-  //time = the value which we provide to set, here timeRemaining - 1;
   set timeRemaining(time) {
     this.durationInput.value = time;
   }
@@ -38,4 +46,15 @@ const durationInput = document.querySelector("#duration");
 const startBtn = document.querySelector("#start");
 const pauseBtn = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startBtn, pauseBtn);
+const timer = new Timer(durationInput, startBtn, pauseBtn, {
+  //notify outside world to trigger events -> for animation
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("Timer just ticked down");
+  },
+  onComplete() {
+    console.log("Timer just completed");
+  },
+});
